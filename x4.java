2a56e5b838bc92549cc619414aa2fc4e4a32b157
lxve-idxl <reid.j.alexander@gmail.com>
2015-10-14 19:41:32 -0400

    Update x4.java

--- x4.java
+++ x4.java
@@ -1,63 +1,55 @@
-//// Exercise 4:  click button to change background.
-
-int r,g,b;
-float button1X=100, button1Y=100, button1W=80, button1H=40;
-int counter=0;
-
-//// SETUP:  size only.  Also set colors.
+////Alexander Reid
+////Color-Changing Button
 void setup() {
-  size( 640, 480 );
-  reset();
+  size (640, 480); 
+  rectMode (CENTER);
+  buttondx = 3;
+  buttondy = 2;
 }
-void reset() {
-  r=  100;
-  g=  200;
-  b=  250;
+ float buttonX = 320;
+ float buttonY = 240;
+ float buttonW = 150;
+ float buttonH = 80;
+ float R = 100;
+ float G = 200;
+ float B = 250;
+ float buttondx;
+ float buttondy;
+ 
+ void draw () {
+   background ( R, G, B); 
+   rect( buttonX, buttonY, buttonW, buttonH);
+   fill(0);
+   text ("Click Me!", buttonX-25, buttonY+2);
+   fill(255); 
+   
+   buttonX += buttondx;
+   buttonY += buttondy;
+ 
+ if(buttonX>width-20) { buttondx= -buttondx; } //reverses motion when hitting borders
+  if(buttonX<1) { buttondx= -buttondx; }
+
+  buttonY=  buttonY + buttondy;  
+  if (buttonY<20 || buttonY>height-20) {
+    buttondy= -buttondy;
+ 
+ }
+ }
+
+void mousePressed (){
+ 
+buttondx = random (-6, +6);
+buttondy = random (-4, +4);
+  
+  
+ if ((mouseX < buttonX + buttonW/2) &&
+    (mouseX > buttonX - buttonW/2) &&
+    (mouseY < buttonY + buttonY/2) &&
+    (mouseY > buttonY - buttonY/2))
+{
+  R = random(255);
+  G = random(255);
+  B = random (255);
 }
 
-
-//// NEXT:  button only.
-void draw() {
-  background( r,g,b );
-  showButton( button1X, button1Y, button1W, button1H );
-  fill(100,0,0);
-  text( "Click me!", button1X+button1W/4, button1Y+button1H*2/3 );
-}
-// Draw the button.
-void showButton( float x, float y, float w, float h ) {
-  fill( 255,255,0 );
-  rect ( x,y, w,h );
-}
-
-//// HANDLERS:  keys & click
-void keyPressed() {
-  if (key == 'q') exit();
-  if (key == 'r') reset();
-}
-void mousePressed() {
-  if ( hit( mouseX,mouseY, 100,100, 50,50 ) ) {
-    counter=  counter+1;
-    if (counter % 2 > 0) {
-      r=  255;
-      g=  50;
-      b=  0;
-    } else {
-      reset();
-    }
-  }
-}
-
-//// OTHER METHODS:  detect "hit"
-// Return true if "near"
-boolean hit( float x1, float y1, float x2, float y2, float w, float h ) {
-  boolean result;
-
-  // +++++ STUB ALWAYS RETURNS TRUE!
-  if ( abs(x1-x2) < w && abs(y1-y2)<h ) {
-    result=  true;
-  } else {
-    result=false;
-  }
-
-  return result;
 }
